fba659f20cba18980dded8450a79ac236e77aebd
Camila Penha <camilaspenha@gmail.com>
2021-09-25 11:52:08 -0300

    [add]Banner Bottom

--- index.php
+++ index.php
@@ -4,5 +4,11 @@ the_post();
 
 get_header();
 
- 
+?>
+ <section class="home__banner--bottom">
+    <?php wptutsplus_home_page_banner('bottom'); ?>
+      
+      
+  </section>
+<?php 
 get_footer();
