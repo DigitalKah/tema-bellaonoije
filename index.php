371a2da80944b6be7a395ffad1277007e97fb50a
Camila Penha <camilaspenha@gmail.com>
2021-09-25 12:01:40 -0300

    [add]Box Img Feature

--- index.php
+++ index.php
@@ -5,9 +5,17 @@ the_post();
 get_header();
 
 ?>
- <section class="home__banner--bottom">
-    <?php wptutsplus_home_page_banner('bottom'); ?>
+  <section class="home__box-features">
+    <div class="container">
+      <div class="box-features__content text-center">
+        <img src="<?php the_field('home_imagem_destaque'); ?>" alt="<?php the_field('home_imagem_destaque_alt'); ?>" class="box-features__img">			
+      </div>
+    </div>
+	</section>
+
+  <section class="home__banner--bottom">
     
+    <?php wptutsplus_home_page_banner('bottom'); ?>    
     <?php get_action_buttons('bottom')?>
       
   </section>
